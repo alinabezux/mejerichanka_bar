411a31ccc0914887526fab01b06a0bfc049c6b46
alinabezux <alinabezux@gmail.com>
2024-11-29 22:28:28 +0200

    url

--- server/configs/configs.js
+++ server/configs/configs.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: process.env.HOST,
-    PORT: process.env.PORT || 5000,
-    MONGO_URL: process.env.MONGO_URL || 'mongodb://127.0.0.1:27017/mejerichanka',
+    PORT: process.env.PORT,
+    MONGO_URL: process.env.MONGO_URL,
     ACCESS_SECRET: process.env.ACCESS_SECRET,
     REFRESH_SECRET: process.env.REFRESH_SECRET,
 
