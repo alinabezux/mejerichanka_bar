accaaf577e4eb1ba3a3d62bc64c74fe5fe25f3c7
alinabezux <alinabezux@gmail.com>
2024-11-29 22:40:19 +0200

    ++

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const ApiError = require("./errors/ApiError");
 
 const app = express();
 
-app.use(cors({ origin: "*"}));
+app.use(cors({ origin: "https://mejerichanka-bar-api.vercel.app"}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'static')));
