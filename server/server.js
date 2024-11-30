727471ce3be9a9ab1b2256d7dc52ae82fea1ce38
alinabezux <alinabezux@gmail.com>
2024-11-30 15:28:55 +0200

    _

--- server/server.js
+++ server/server.js
@@ -12,12 +12,7 @@ const ApiError = require("./errors/ApiError");
 
 const app = express();
 
-app.use(cors({
-    origin: ['https://mejerichanka.vercel.app', 'http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-}));
-
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'static')));
@@ -25,11 +20,6 @@ app.use(express.static(path.resolve(__dirname, 'static')));
 app.use(fileUpload());
 
 app.use('/api', router);
-app.use('/api2', (req, res, next) => {
-    console.log(`Request to ${req.originalUrl}`);
-    next();
-});
-
 
 app.get('/', (req, res) => {
     res.json({ message: "WELCOME" });
