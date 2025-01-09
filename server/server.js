07da25633d854d6eb18ef74d9ca3b70b84c068c3
alinabezux <alinabezux@gmail.com>
2025-01-09 20:31:28 +0200

    cors

--- server/server.js
+++ server/server.js
@@ -12,10 +12,7 @@ const ApiError = require("./errors/ApiError");
 
 const app = express();
 
-app.use(cors({
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-    origin: configs.CLIENT_URL,
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.resolve(__dirname, 'static')));
